c39ee840181d52b9a6f70319e63147aab1ab019d
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-10-15 21:57:38 -0400

    got spotify working, got omdb working

--- liri.js
+++ liri.js
@@ -53,19 +53,41 @@ function mySwitch(command) {
     }
 }
 
+function concertThis() {
+    //concert-this
+    //use BandsInTown API to get name of venue, venue location, date of event
+    var artist = searchFor;
+    var bandsUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    console.log(bandsUrl);
+    //run request with axios to BandsInTown API
+    axios.get(bandsUrl).then(
+        function (response) {
+            // If the request is successful = 200
+            if (!error && response.statusCode === 200) {
+                var body = JSON.parse(body);
 
-//concert-this
-//use BandsInTown API to get name of venue, venue location, date of event
-var artist = searchFor;
-var bandsUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-console.log(bandsUrl);
-//run request with axios to BandsInTown API
+                //Simultaneously output to console and log.txt via NPM simple-node-logger
+                log('================ Movie Info ================');
+                logO("Title: " + body.Title);
+                logOutput("Release Year: " + body.Year);
+                logOutput("IMdB Rating: " + body.imdbRating);
+                logOutput("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+                logOutput("Country: " + body.Country);
+                logOutput("Language: " + body.Language);
+                logOutput("Plot: " + body.Plot);
+                logOutput("Actors: " + body.Actors);
+                logOutput('==================END Movie Info=================');
 
-
+            } else {
+                console.log("There has been an error", error.message);
+            }
+        }
+    );
+}
 
 //spotify-this-song
-var spotifyThis = function (song) {
-    if (song === undefined) {
+function spotifyThis(song) {
+    if (!song) {
         song = "The Sign";
     }
 
@@ -81,64 +103,65 @@ var spotifyThis = function (song) {
             }
 
             var songData = data.tracks.items;
+            //console.log(songData);
+            //console.log(songData[0]);
             for (var i = 0; i < songData.length; i++) {
-                console.log("Artist: " + songData.artists[0].name);
-                console.log("Song: " + songData.name);
-                console.log("Preview URL: " + songData.preview_url);
-                console.log("Album: " + songData.album.name);
+                console.log("Artist: " + songData[i].artists[0]);
+                console.log("Song: " + songData[i].name);
+                console.log("Preview URL: " + songData[i].preview_url);
+                console.log("Album: " + songData[i].album.name);
                 console.log("----------------------------");
 
                 //adds text to log.txt
-                fs.appendFile('log.txt', songData.artists[0].name);
-                fs.appendFile('log.txt', songData.name);
-                fs.appendFile('log.txt', songData.preview_url);
-                fs.appendFile('log.txt', songData.album.name);
-                fs.appendFile('log.txt', "-----------------------");
+                // fs.appendFile('log.txt', songData.artists[0].name);
+                // fs.appendFile('log.txt', songData.name);
+                // fs.appendFile('log.txt', songData.preview_url);
+                // fs.appendFile('log.txt', songData.album.name);
+                // fs.appendFile('log.txt', "-----------------------");
             }
         });
 }
 
 //OMDB Movie command: movie-this
 //include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-var movieName = searchFor;
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+function movieThis() {
+    var movieName = searchFor;
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
 
-// test queryUrl
-console.log(queryUrl);
+    // test queryUrl
+    console.log(queryUrl);
 
-//run a request with axios to OMDB API 
-axios.get(queryUrl).then(
-  function(response) {
-   
-    request(queryUrl, function (error, response, body) {
-
-        // If the request is successful = 200
-        if (!error && response.statusCode === 200) {
-            var body = JSON.parse(body);
-
-            //Simultaneously output to console and log.txt via NPM simple-node-logger
-            logOutput('================ Movie Info ================');
-            logOutput("Title: " + body.Title);
-            logOutput("Release Year: " + body.Year);
-            logOutput("IMdB Rating: " + body.imdbRating);
-            logOutput("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
-            logOutput("Country: " + body.Country);
-            logOutput("Language: " + body.Language);
-            logOutput("Plot: " + body.Plot);
-            logOutput("Actors: " + body.Actors);
-            logOutput('==================END Movie Info=================');
-
-         } else {
-      console.log("There has been an error", error.message);
-    }
-    
-    //Response if user does not type in a movie title
-    if (movieName === "Mr. Nobody") {
+    //if no input
+    if (!movieName) {
+        movieName = "Mr. Nobody"
         console.log("-----------------------");
         console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
         console.log("It's on Netflix!");
     }
-  });
+
+
+    //run a request with axios to OMDB API 
+    axios.get(queryUrl).then(
+
+        function (response) {
+            var body = (response.data);
+
+            //Simultaneously output to console and log.txt via NPM simple-node-logger
+            console.log('================ Movie Info ================');
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[0].Value);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log('==================END Movie Info=================');
+        })
+        .catch(function (error) { console.log(error) })
+
+}
+
 
 // For command: do-what-it-says
 // takes text inside of random.txt then use it to call one of LIRI's commands
